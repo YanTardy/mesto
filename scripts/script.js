254afea73d9a74155c58d25afd932cd0bf4bbc93
Yan Dolgodush <y.dolgodush@yandex.ru>
2022-09-01 12:38:09 +0300

    fix: внесены правки после второго ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let popupClose = document.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_description');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
@@ -11,11 +11,11 @@ function popupOpenAction() {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileDescription.textContent;
-};
+}
 
 function popupCloseAction() {
   popup.classList.remove('popup_opened');
-};
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
